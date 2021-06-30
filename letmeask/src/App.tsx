92712f258eb200d18167f242df8d8ec774ac9810
Gustavo-Carmo1 <gucoelho21@gmail.com>
2021-06-29 23:28:53 -0400

    Mudanças dia 02

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,10 +1,24 @@
-import {Button} from './components/Button'
+// Importações React
+import { BrowserRouter,Route } from 'react-router-dom'
+
+// Importações de páginas
+import { NewRoom } from './pages/NewRoom';
+import { Home } from './pages/Home';
+
+// Importação de Contextos
+import { AuthContextProvider } from './contexts/AuthContexts';
+
+
 
 function App() {
+
   return (
-    <div>
-      <Button/>
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider> 
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
--- letmeask/src/services/firebase.ts
+++ letmeask/src/services/firebase.ts
@@ -4,16 +4,18 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyAkPWqK7JsdwxGZRCRUtiCmRIHq5lbqrxc",
+    authDomain: "letmeask-ed61d.firebaseapp.com",
+    databaseURL: "https://letmeask-ed61d-default-rtdb.firebaseio.com",
+    projectId: "letmeask-ed61d",
+    storageBucket: "letmeask-ed61d.appspot.com",
+    messagingSenderId: "972863108648",
+    appId: "1:972863108648:web:98b43ff69a8efb05e9671d"
   };
 
   firebase.initializeApp(firebaseConfig);
 
-  export const auth = firebase.auth();
-  export const database = firebase.database();
+   const auth = firebase.auth();
+   const database = firebase.database();
+  
+   export {firebase, auth, database}
